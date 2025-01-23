31be1dd2da5e4d62e1c7581e5f9a3fcdc06312a0
Amanda Robles <amandarobles@usf.edu>
2025-01-23 17:58:23 -0500

    Task 2- Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
 console.log("Is Active:", isActive, "Type:", typeof isActive);
+
+// task 2 - Arithmetic Operations
+let productName = "Laptop";
+const productPrice = 900;
+var isAvailable = true;
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
