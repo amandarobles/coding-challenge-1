b68a11bccdb662f54d1e40ad88720edf2855c1a8
Amanda Robles <amandarobles@usf.edu>
2025-01-23 17:40:55 -0500

    Task 1- Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+// task 1 - Variable
+let employeeName = new String("JohnDoe");
+const employeeID = "00001";
+var isActive = true;
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+console.log("Is Active:", isActive, "Type:", typeof isActive);
